5709b98a0c951113c262c2339e9177bebae453b6
joeyf123 <61664460+joeyf123@users.noreply.github.com>
2021-01-16 14:14:12 -0600

    deploy

--- src/app/photography/photography-items.ts
+++ src/app/photography/photography-items.ts
@@ -1,8 +1,10 @@
 import {PhotographyItem} from './photography-item';
 
 export const PhotographyItems: PhotographyItem[] = [
-  {  typeOfShot: 'typeOfShot',
-    price: 'price',
-    description: 'description',
-    imagePath: 'imagePath'},
+  {  typeOfShot: 'Senior pics', price: '$50', description: 'description', imagePath: 'assets/img.png'},
+  {  typeOfShot: 'Family shoots', price: '$100', description: 'description', imagePath: 'assets/img.png'},
+  {  typeOfShot: 'Events***', price: '$150', description: 'description', imagePath: 'assets/img.png'},
+  {  typeOfShot: 'Pets', price: '$50', description: 'description', imagePath: 'assets/img.png'},
+  {  typeOfShot: 'Holiday shoot', price: '$50', description: 'description', imagePath: 'assets/img.png'},
+  {  typeOfShot: 'Couple shoot', price: '$75', description: 'description', imagePath: 'assets/img.png'},
 ];
